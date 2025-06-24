92786c50beed4edde7b2963096bcad5200413e4b
keith nolan <keith.nolan@bitflex.com>
2025-06-24 14:42:13 +0100

    Set up minimal apis, vertical slice, cqrs implementation, repository pattern, postgres with marten library for basket Api

---
+++ src/Services/Basket/Basket.API/Exceptions/BasketNotFoundException.cs
@@ -0,0 +1,11 @@
+namespace Basket.API.Exceptions;
+public class BasketNotFoundException: NotFoundException
+{
+    public BasketNotFoundException(string userName) : base("Basket", userName)
+    {
+    }
+
+    public BasketNotFoundException(string name, object key) : base(name, key)
+    {
+    }
+}
---
+++ src/Services/Basket/Basket.API/Program.cs
@@ -0,0 +1,26 @@
+
+using BuildingBlocks.Exceptions.Handler;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var assembly = typeof(Program).Assembly;
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehaviour<,>));
+});
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+var app = builder.Build();
+
+app.MapCarter();
+app.UseExceptionHandler(options => {});
+app.Run();
