94ea9fb146ff0f9a8ef434c962ce5d30eff0f04a
keith nolan <keith.nolan@bitflex.com>
2025-07-01 11:32:38 +0100

    Developed Strongly typed IDs for Order Entities

---
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/OrderName.cs
@@ -0,0 +1,20 @@
+namespace Ordering.Domain.ValueObjects;
+
+public record OrderName
+{
+    private const int DefaultLength = 5;
+    public string Value { get; }
+    
+    public OrderName(string value) => Value = value;
+
+    public static OrderName Of(string value)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(value);
+        ArgumentOutOfRangeException.ThrowIfNotEqual(value.Length, DefaultLength);
+        
+        return new OrderName(value);
+    }
+    
+    
+    
+}
