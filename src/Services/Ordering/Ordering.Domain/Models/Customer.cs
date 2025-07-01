78604ba92ae70e8c0643fb8c9090b5d7963b0196
keith nolan <keith.nolan@bitflex.com>
2025-07-01 10:28:28 +0100

    Develop Domain Modeling with DDD for Ordering Microservices

---
+++ src/Services/Ordering/Ordering.Domain/Models/Customer.cs
@@ -0,0 +1,9 @@
+using Ordering.Domain.Abstractions;
+
+namespace Ordering.Domain.Models;
+
+public class Customer : Entity<Guid>
+{
+    public string Name { get; set; } = default!;
+    public string Email { get; set; } = default!;
+}
